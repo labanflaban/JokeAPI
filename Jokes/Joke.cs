264e30e2dc768d147d7641e3f0d5f17cd9a2c5d4
Ghazi Hicheri <ghazi123@hotmail.se>
2024-01-09 13:29:53 +0100

    first commit

---
+++ Jokes/Joke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Jokes
+{
+    internal class Joke
+    {
+        public bool Error { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string? joke { get; set; }
+        public string? Setup { get; set; }
+        public string? Delivery { get; set; }
+
+        public Flags Flags { get; set; }
+
+        public bool Safe { get; set; }
+        public int Id { get; set; }
+        public string Lang { get; set; }
+    }
+}
---
+++ Jokes/Program.cs
@@ -0,0 +1,74 @@
+﻿using Jokes;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+
+
+class Program
+{
+
+    HttpClient client = new HttpClient();
+    static async Task Main()
+    {
+        Program program = new Program();
+
+        Console.WriteLine("1. Deserialize a joke\n2. Serialize a joke");
+        string input = "-1";
+        while (input != "0")
+        {
+            
+            input = Console.ReadLine();
+            if (input == "1")
+            {
+                await program.Deserialize();
+                Console.WriteLine("desiralization complete");
+
+            }
+            else if (input == "2")
+            {
+                program.serialization();
+                Console.WriteLine("Serialization complete");
+            }
+            Console.WriteLine("1. Deserialize a joke\n2. Serialize a joke");
+        }
+      
+
+    }
+    //Deserialize from the 
+    private async Task Deserialize()
+    {
+      
+        string fileName = "joke.json";
+        string jsonString = File.ReadAllText(fileName);
+        Joke joke = JsonSerializer.Deserialize<Joke>(jsonString)!;
+        Console.WriteLine(joke.joke);
+
+        Console.WriteLine(joke.Setup);
+
+        Console.WriteLine(joke.Delivery);
+
+    }
+    //Serialize data from the API
+    private async Task serialization()
+    {
+       
+        
+        client = new()
+        {
+            BaseAddress = new Uri("https://v2.jokeapi.dev")
+        };
+
+        Joke? joke = await client.GetFromJsonAsync<Joke>("/joke/any");
+
+      
+        var options = new JsonSerializerOptions();
+        options.WriteIndented = true;
+        string jsonString = JsonSerializer.Serialize<Joke>(joke, options);
+        File.WriteAllText("joke.json", jsonString);
+    }
+}
+
